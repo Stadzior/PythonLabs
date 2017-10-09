429a855ba401e1b5c437f4dd496edf7c4938cf2c
Stadzior <Stadzior@gmail.com>
2017-10-09 18:22:22 +0200

    Task 2 almost done.

---
+++ zadanie2.py
@@ -0,0 +1,14 @@
+import os
+
+n = input("Input a number:")
+for i in range(0,n):
+    name = input("Give me a name and surname e.g. Jane Plain:")
+os.system("cls")
+print("-------------------\n")
+print("-Choose an option:-\n")
+print("-------------------\n")
+print("1. Sort by names\n")
+print("2. Sort by surname\n")
+x = input("")
+if x
+    
