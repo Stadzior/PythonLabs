734333c1d90f4b2eb5aee23f19dc11333e72fa37
Stadzior <Stadzior@gmail.com>
2017-10-09 17:41:19 +0200

    3 goats parable

--- zadanie1.py
+++ zadanie1.py
@@ -2,9 +2,13 @@ for i in range(1,9):
     print (pow(2,i))
 
 for i in range(1,6):
-    answer=int(input("Give me 3 to {}:".format(i)))
-    if answer != pow(3,i):
-        print ("Sorry, 3 to {} equals {} not {}, good bye.".format(i,pow(3,i),answer))
-        break
+    answer=input("Give me 3 to {}:".format(i))
+    boostedAnswer=""
+    for x in answer:
+        if x.isdigit():
+            boostedAnswer+=x
+    if int(boostedAnswer) != pow(3,i):
+        print ("Sorry, 3 to {} equals {} not {}, good bye.".format(i,pow(3,i),boostedAnswer))
+        break    
         
 
