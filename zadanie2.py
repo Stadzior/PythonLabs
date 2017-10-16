4702dd65b5422c9b70db1c747b311385392e9a5a
Stadzior <Stadzior@gmail.com>
2017-10-16 17:44:06 +0200

    Added logic for menu.

--- zadanie2.py
+++ zadanie2.py
@@ -1,14 +1,26 @@
 import os
 
-n = input("Input a number:")
+n = int(input("Input a number:"))
+persons = []
 for i in range(0,n):
-    name = input("Give me a name and surname e.g. Jane Plain:")
-os.system("cls")
-print("-------------------\n")
-print("-Choose an option:-\n")
-print("-------------------\n")
-print("1. Sort by names\n")
-print("2. Sort by surname\n")
-x = input("")
-if x
+    persons.append(input("Give me a name and surname e.g. Jane Plain:"))
+x = -1
+while x != 1 or x != 2:
+    os.system("cls")
+    print("-------------------\n")
+    print("-Choose an option:-\n")
+    print("-------------------\n")
+    print("1. Sort by name\n")
+    print("2. Sort by surname\n")
+    x = int(input(""))
+if x == 1:
+    persons = sorted(persons, key=lambda person: person[0])
+    print("X == 1")
+else:
+    if x == 2:
+        print("X == 2")
+        persons = sorted(persons, key=lambda person: person[1])
+for person in persons:
+    print(person)
+    print("WUT")
     
