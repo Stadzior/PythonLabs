16cdbd2e5c4925885ad09460231ba43b69628716
Stadzior <Stadzior@gmail.com>
2017-10-16 18:14:17 +0200

    task done briefly

--- zadanie2.py
+++ zadanie2.py
@@ -1,26 +1,21 @@
-import os
-
 n = int(input("Input a number:"))
 persons = []
 for i in range(0,n):
-    persons.append(input("Give me a name and surname e.g. Jane Plain:"))
-x = -1
-while x != 1 or x != 2:
-    os.system("cls")
-    print("-------------------\n")
-    print("-Choose an option:-\n")
-    print("-------------------\n")
-    print("1. Sort by name\n")
-    print("2. Sort by surname\n")
-    x = int(input(""))
-if x == 1:
-    persons = sorted(persons, key=lambda person: person[0])
-    print("X == 1")
-else:
-    if x == 2:
-        print("X == 2")
-        persons = sorted(persons, key=lambda person: person[1])
-for person in persons:
-    print(person)
-    print("WUT")
-    
+    persons.append(input("Give me a name and surname e.g. Jane Plain:").split())
+x = "0"
+while x != "q":
+    x  = "0"
+    while x != "1" and x != "2":
+        print("-------------------\n")
+        print("-Choose an option:-\n")
+        print("-------------------\n")
+        print("1. Sort by name\n")
+        print("2. Sort by surname\n")
+        x = input("")
+    if x == "1":
+        sortedPersons = sorted(persons, key=lambda person: person[0])
+    else:
+        if x == "2":
+            sortedPersons = sorted(persons, key=lambda person: person[1])
+    for person in sortedPersons:
+        print(person)
