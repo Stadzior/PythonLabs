0c393afa322ca6105d778df034541f4036bbb8d8
Stadzior <Stadzior@gmail.com>
2017-10-31 19:47:36 +0100

    Menu centered.

--- zadanie2.py
+++ zadanie2.py
@@ -1,3 +1,5 @@
+import shutil
+columns = shutil.get_terminal_size().columns
 n = int(input("Input a number:"))
 persons = []
 for i in range(0,n):
@@ -6,12 +8,12 @@ x = "0"
 while x != "q":
     x  = "0"
     while x != "1" and x != "2" and x != "q":
-        print(" --------------------")
-        print("| Choose an option:  |")
-        print(" --------------------")
-        print("| 1. Sort by name    |")
-        print("| 2. Sort by surname |")
-        print(" --------------------")
+        print(" --------------------".center(columns))
+        print("| Choose an option:  |".center(columns))
+        print(" --------------------".center(columns))
+        print("| 1. Sort by name    |".center(columns))
+        print("| 2. Sort by surname |".center(columns))
+        print(" --------------------".center(columns))
         x = input("")
     if x == "1":
         sortedPersons = sorted(persons, key=lambda person: person[0])
@@ -20,5 +22,5 @@ while x != "q":
             sortedPersons = sorted(persons, key=lambda person: person[1])
     if (x == "1" or x =="2"):
         for i in range(0, len(sortedPersons)):
-            print('{}. {} {}'.format(i+1,sortedPersons[i][0],sortedPersons[i][1]))
+            print('{}. {} {}'.format(i+1,sortedPersons[i][0],sortedPersons[i][1]).center(columns))
     
