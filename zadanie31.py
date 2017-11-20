b9ff01048dca6bc69ee510707b2d41fcf7f7184e
Stadzior <Stadzior@gmail.com>
2017-11-20 10:24:03 -0500

    3.1 implementation2

--- zadanie31.py
+++ zadanie31.py
@@ -11,6 +11,7 @@ class MainForm(npyscreen.ActionForm):
 	ms = None
 	db = None
 	gd = None
+	loadedSeparator = None
 	def create(self):
 		self.ms = self.add(npyscreen.TitleSelectOne, max_height=7, value = [0,], name="Wybierz opcje:", values = ["Laduj baze z pliku","Zapisz baze do pliku", "Dodaj nowy wpis do bazy", "Usun wpis z bazy"], scroll_exit=True)
 		self.gd = self.add(npyscreen.GridColTitles, relx = 20, rely=10, width=50, col_titles = ['Imie', 'Nazwisko'])
@@ -21,6 +22,7 @@ class MainForm(npyscreen.ActionForm):
 				self.db = []
 				with open(npyscreen.selectFile(),'r') as f:
 					content = f.readlines()
+				loadedSeparator = self.get_separator(content[0])
 				content = self.normalize_data(content)
 				for row in content:
 					self.db.append(row.strip().split("#"))
@@ -50,20 +52,23 @@ class MainForm(npyscreen.ActionForm):
 	
 	def get_separator(self, row):
 		for char in row:
-			if not (char.isalpha() || char == '-'):
+			if not (char.isalpha() or char == '-'):
 				return char
 
 	def normalize_data(self, content):
 		for row in content:
 			separator = self.get_separator(row)
-			for i in range (1,len(row)):
+			for i in range (1,len(content)):
+				print('{} {} {}'.format(i,)
 				char = row[i]
-				if not char.isalpha() && char != '-' && char != separator:
+				if not char.isalpha() and char != '-' and char != separator:
 					row = row.replace(char,'')
 				else:
 					prevChar = row[i-1]
-					if char == separator && prevChar == separator:
+					if char == separator and prevChar == separator:
 						row = row[:i] + row[i:]
+				row = row.replace(char,'#')
+		return content
 							
 
 class AddRecordForm(npyscreen.ActionPopup):
